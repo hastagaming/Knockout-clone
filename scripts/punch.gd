51d0dcedb3957769c48c2f5e69ce9d72531730f6
hastagaming <rip.hastagaming@gmail.com>
2026-03-19 10:53:15 +0700

    Delete punch.gd

--- scripts/punch.gd
+++
@@ -1,9 +0,0 @@
-extends Area3D
-
-@export var force := 20.0
-
-func _on_body_entered(body):
-    if body is CharacterBody3D:
-        var dir = (body.global_position - global_position).normalized()
-        body.velocity += dir * force
-        body.velocity.y = 6
